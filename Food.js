e65124abb73211e540123cf4d327e2be7a1d22a5
sohamthecoder123 <69463653+sohamthecoder123@users.noreply.github.com>
2020-10-17 20:33:09 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,18 @@
+class Food{
+    constructor(){
+        var foodstock, lastFed;
+        this.image = loadImage("Milk.png");
+    }
+    getFoodStock(){
+
+    }
+    updateFoodStock(){
+
+    }
+    deductFood(){
+
+    }
+    display(){
+        
+    }
+}
